728908e56d38cd801e64bb18940971a1bfe6d129
jby <1614963384@qq.com>
2017-11-29 16:34:44 +0800

    实现功能:下拉滚动条时,页面下方加载新图片

--- waterfall_jQuery.js
+++ waterfall_jQuery.js
@@ -2,7 +2,7 @@
 // var main=document.getElementById('main');
 //  var boxArr=$('.box');
 // var picArr=document.getElementsByClassName('pic');
-// var newPicture={"data":[{'src':"1.jpg"},{'src':"2.jpg"},{'src':"3.jpg"}]};
+ var newPicture={"data":[{'src':"1.jpg"},{'src':"2.jpg"},{'src':"3.jpg"}]};
 //下一行的图片跟在这行高度值最小的图片后面
 function changePosition() {
     var heightArr = [];//创建一个数组,来存放每一列的图片高度.
@@ -25,4 +25,26 @@ function changePosition() {
         }
     });
 }
-changePosition();
+//添加图片
+function check(){
+    var $lastBox=$('#main').find('>div').last();
+    var lastBoxDis=$lastBox.offset().top+Math.floor($lastBox.outerHeight()/2);
+    var scroll=$(window).scrollTop();
+    var documentH=$(window).height();
+    return (lastBoxDis < scroll + documentH);
+}
+function scroll(){
+    changePosition();
+    //监听滚动事件
+    $(window).scroll(function(){
+        if(check){
+            $.each(newPicture.data,function(key,value){
+                var oBox=$('<div>').addClass('box').appendTo($('#main'));
+                var oPic=$('<div>').addClass('pic').appendTo($(oBox));
+                $('<img>').attr('src','img/'+$(value).attr('src')).appendTo($(oPic));
+            });
+            changePosition();
+        }
+    })
+}
+scroll();
